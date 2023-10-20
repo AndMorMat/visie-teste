42a652ed262561f57d300b0d610ae5fcb24f3f0e
Andre Matos <andre_matos13@hotmail.com>
2023-10-20 11:53:42 -0300

    Ajusta o redirecionamento apos o cadastro de uma pessoa

--- visie-app/src/ui/screens/AddPersonScreen/index.tsx
+++ visie-app/src/ui/screens/AddPersonScreen/index.tsx
@@ -4,12 +4,20 @@ import { FormPerson } from '../../components/common/FormPerson'
 import { usePeopleContext } from '../../../contexts/people-context'
 import { PeopleType } from '../../../types/people'
 import styles from './styles'
+import { PEOPLE_LIST_ROUTE_NAME, StackParamListType } from '../../../types/navigation'
+import { NavigationProp, useNavigation } from '@react-navigation/native'
 
 export function AddPersonScreen() {
+  const navigation = useNavigation<NavigationProp<StackParamListType>>()
   const { addPerson } = usePeopleContext()
+
+  const goToPersonListScreen = () => {
+    navigation.navigate(PEOPLE_LIST_ROUTE_NAME)
+  }
 
   const handleAddPerson = (person: PeopleType) => {
     addPerson(person)
+    goToPersonListScreen()
   }
 
   return (
